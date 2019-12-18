0a41a3926b18f7259d41725122422a4d0dfc1034
gesa30527 <58959044+gesa30527@users.noreply.github.com>
2019-12-18 02:34:11 +0100

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
     if(!message.guild) return;
-    if(!message.member.roles.find(r => r.name === "Owner" || r.name === "Administrator")) return message.reply('You dont have permission to use this command!');
+    if(!message.member.roles.find(r => r.name === "Founder" || r.name === "Administrator"|| r.name === "Moderators")) return message.reply('You dont have permission to use this command!');
         let msgArgs = args.join(" ");
         if(!msgArgs) return message.reply("You need to specify a text!")
         message.channel.send(msgArgs).then(messageReaction => {
