b597c94c4e436fb6d4a9f71c4939fe36835535ee
gesa30527 <58959044+gesa30527@users.noreply.github.com>
2019-12-18 03:54:58 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { getMember, formatDate } = require("../functions.js");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.guild) return;
