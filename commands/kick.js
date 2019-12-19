864e03bb26256380d76bb968f0024dfa5cbc5fbe
gesa30527 <58959044+gesa30527@users.noreply.github.com>
2019-12-19 14:33:47 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,10 +6,10 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.roles.find(r => r.name === "Moderators" || r.name === "Founder" || r.name === "Administrator" || r.name === "Moderator Leader")) return message.reply('You dont have permission to use this command!');
     let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let reason = args.slice(1).join(' ');
-    let logs = message.guild.channels.find(ch => ch.name === "logs");
+    let logs = message.guild.channels.find(ch => ch.name === "🔍mod-logs");
     if (!target) return message.reply('You need to specify a member!');
     if (!reason) return message.reply('You need to specify a reason!');
-    if (!logs) return message.reply(`Cloudn't find channel ``logs```);
+    if (!logs) return message.reply(`Cloudn't find channel ``🔍mod-logs```);
     let embed = new Discord.RichEmbed()
     .setColor('BLUE')
         .setThumbnail(target.user.avatarURL)
